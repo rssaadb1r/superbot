50e3667f41474d181b621b0900b435f1dda1a498
saadmzed <saad5mzed@gmail.com>
2017-05-14 18:43:49 +0300

    Add files via upload

---
+++ data/config.lua
@@ -0,0 +1,23 @@
+do local _ = {
+  admins = {},
+  disabled_channels = {},
+  enabled_plugins = {
+    "banhammer",
+    "groupmanager",
+    "msg-checks",
+    "plugins",
+    "tools",
+    "fun",
+    "update",
+    "help"
+  },
+  info_text = "》Beyond Reborn V5.0\nAn advanced administration bot based on https://valtman.name/telegram-cli\n\n》https://github.com/BeyondTeam/BDReborn\n\n》Admins :\n》@SoLiD ➣ Founder & Developer《\n》@Makan ➣ Developer & Sponser《\n》@Rixel ➣ Developer 《\n》@Exacute ➣ Developer《\n》@ToOfan ➣ Developer《\n》@CiveY ➣ Developer\n》@K_a_I_i_I_i_n_u_x ➣ Developer《\n》@CliFather ➣ Developer《\n\n》Special thanks to :\n》@Vysheng\n》@MrHalix\n》@Nero_Dev\n》And Beyond Team Members\n\n》Our channel :\n》@BeyondTeam《\n\n》Our website :\n》http://BeyondTeam.ir\n",
+  moderation = {
+    data = "./data/moderation.json"
+  },
+  sudo_users = {
+    30742221
+  }
+}
+return _
+end
